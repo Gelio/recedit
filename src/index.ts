dbd5e170ae3c44e8faee828c00030a51ded157fc
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-10-07 22:00:32 +0200

    add drawing lines in index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
 import 'index.scss';
 import 'normalize.css';
 
-console.log('Typescript working and transpiled successfully');
+import { COLORS } from 'common/COLORS';
+import { Point } from 'common/Point';
+import { LineRasterizer } from 'LineRasterizer';
+import { Renderer } from 'Renderer';
+
+window.addEventListener('load', bootstrap, false);
+
+function bootstrap(): void {
+  const canvas = <HTMLCanvasElement>document.getElementById('main-canvas');
+  const renderer = new Renderer(canvas, { lineRasterizer: new LineRasterizer() });
+
+  setInterval(() => {
+    const x = Math.random() * canvas.clientWidth;
+    const y = Math.random() * canvas.clientHeight;
+    const startPoint = new Point(x, y);
+    const endPoint = new Point(x + 40, y + 10);
+
+    renderer.drawLine(startPoint, endPoint, { color: COLORS.RED, thickness: 2 });
+  }, 250);
+}
