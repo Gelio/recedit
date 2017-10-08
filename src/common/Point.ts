702d1994ea4defae9a89f559aa21a35b6b7363b3
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-10-08 12:50:20 +0200

    add octants, add determining octatnt for a point

--- src/common/Point.ts
+++ src/common/Point.ts
@@ -1,3 +1,5 @@
+import { Octant } from 'common/Octant';
+
 export class Point {
   public x: number;
   public y: number;
@@ -13,5 +15,47 @@ export class Point {
 
   public static subtract(p1: Point, p2: Point): Point {
     return new Point(p1.x - p2.x, p1.y - p2.y);
+  }
+
+  public getOctant(): Octant {
+    const x = this.x;
+    const y = this.y;
+    let octant = Octant.First;
+
+    if (y >= 0) {
+      if (x >= 0) {
+        // First quarter
+        if (y <= x) {
+          octant = Octant.First;
+        } else {
+          octant = Octant.Second;
+        }
+      } else {
+        // Second quarter
+        if (y >= -x) {
+          octant = Octant.Third;
+        } else {
+          octant = Octant.Fourth;
+        }
+      }
+    } else {
+      if (x <= 0) {
+        // Third quarter
+        if (y >= x) {
+          octant = Octant.Fifth;
+        } else {
+          octant = Octant.Sixth;
+        }
+      } else {
+        // Fourth quarter
+        if (y < -x) {
+          octant = Octant.Seventh;
+        } else {
+          octant = Octant.Eighth;
+        }
+      }
+    }
+
+    return octant;
   }
 }
