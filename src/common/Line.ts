413be5b2173d2d9eb6a6f727bd3a4de5c9177bef
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-10-12 14:17:57 +0200

    add helper distance method to Line

--- src/common/Line.ts
+++ src/common/Line.ts
@@ -5,7 +5,21 @@ export class Line {
   public p2: Point;
 
   constructor(p1: Point, p2: Point) {
+    if (p1.equals(p2)) {
+      throw new Error('Cannot create line between points at the same coordinates');
+    }
+
     this.p1 = p1;
     this.p2 = p2;
+  }
+
+  public distanceToPoint(p: Point) {
+    const p1 = this.p1;
+    const p2 = this.p2;
+
+    const yDelta = p2.y - p1.y;
+    const xDelta = p2.x - p1.x;
+
+    return Math.abs(yDelta * p.x - xDelta * p.y + p2.x * p1.y - p2.y * p1.x) / Point.getDistanceBetween(p1, p2);
   }
 }
