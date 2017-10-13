06b178a84c96f798be29e3d2d21bdc461f5952de
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-10-13 10:04:04 +0200

    add LineCondition abstract class

--- src/conditions/LineCondition.ts
+++ src/conditions/LineCondition.ts
@@ -1,3 +1,18 @@
 import { Condition } from 'conditions/Condition';
 
-export class LineCondition implements Condition {}
+import { Line } from 'common/Line';
+import { Path } from 'common/Path';
+
+export class LineCondition implements Condition {
+  protected readonly line: Line;
+  protected readonly path: Path;
+
+  constructor(line: Line, path: Path) {
+    this.line = line;
+    this.path = path;
+  }
+
+  public isMet(): boolean {
+    throw new Error('LineCondition not implemented');
+  }
+}
