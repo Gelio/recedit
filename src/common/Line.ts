9efd370812b0041e485bca45d1d4fd0649c946dc
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-10-16 16:44:34 +0200

    add getLength method to Line

--- src/common/Line.ts
+++ src/common/Line.ts
@@ -35,4 +35,8 @@ export class Line {
   public getMiddlePoint(): Point {
     return new Point((this.p1.x + this.p2.x) / 2, (this.p1.y + this.p2.y) / 2);
   }
+
+  public getLength() {
+    return Point.getDistanceBetween(this.p1, this.p2);
+  }
 }
