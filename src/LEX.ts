d0fbfe6e26962bcd59a738f2c4b95f1cb505a41b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-10-17 21:59:53 +0200

    add cancelling path adding

--- src/LEX.ts
+++ src/LEX.ts
@@ -4,7 +4,10 @@ const LEX = {
   PATH_LAYER_NAME: 'PathLayer',
   PATH_GHOST_LAYER_NAME: 'PathGhostLayer',
   NEW_CONDITION_EVENT_NAME: 'new-condition',
-  REMOVE_CONDITION_EVENT_NAME: 'remove-condition'
+  REMOVE_CONDITION_EVENT_NAME: 'remove-condition',
+  KEY_CODE: {
+    ESCAPE: 27
+  }
 };
 
 export {
